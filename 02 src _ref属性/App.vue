dbe7d5ebba258ea30949bd83ee6dd4cdd484b09e
yu-classmate <1914274610@qq.com>
2022-08-24 21:35:13 +0800

    ref属性

---
+++ 02 src _ref属性/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDOM">点我输出上方的DOM元素</button>
+    <img src="./assets/logo.png" alt="logo">
+    <school ref="sch"></school>
+    <student ></student>
+  </div>
+</template>
+<script>
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+  data(){
+    return {
+      msg:"欢迎学习Vue!!!"
+    }
+  },
+  methods:{
+    showDOM(){
+      console.log(this.$refs.title);//得到h1的真实DOM元素
+      console.log(this.$refs.sch);//得到school组件实例对象
+      console.log(this.$refs);
+    }
+  }
+};
+// ref属性
+//1、被用来给元素或子组件注册引用信息（id的替代者）
+//2、应用在html标签上获取的是真实DOM元素，应用在组件标签上获取的是组件实例对象
+//3、使用方式
+// <h1  ref="title"></h1>
+// this.$refs.title
+</script>
+<style></style>
---
+++ 02 src _ref属性/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="demo">
+    <h2>学校名字：{{ schoolName }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "School", 
+  data() {
+    return {
+      schoolName: "尚硅谷",
+      address: "北京",
+    };
+  },
+};
+</script>
+<style>
+.demo {
+  background-color: antiquewhite;
+}
+</style>
