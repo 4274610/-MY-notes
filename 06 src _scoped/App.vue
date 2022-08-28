73cf79ffcbd7ae75c4bfbd37d5bb4808d103d637
yu-classmate <1914274610@qq.com>
2022-08-28 23:05:21 +0800

    scoped

---
+++ 06 src _scoped/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <student></student>
+    <hr>
+    <school></school>
+  </div> 
+</template>
+<script>
+import School from "./components/School.vue";
+
+import Student from "./components/Student.vue";
+export default {
+  name: "App",
+  components: {
+    Student,
+    School
+  }
+  }
+</script>
+<style>
+.title{
+  color:red
+}
+/* style在APP.vue中对全部的title起作用 */
+</style>
---
+++ 06 src _scoped/components/Student.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo"> 
+    <h2 class="title">学生姓名：{{ name }}</h2>
+    <h2 >学生性别：{{ sex }}</h2>
+
+  
+  </div>
+</template>
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+     name:"张三",
+     sex:"男",
+     age:19
+    };
+  },
+}
+
+</script>
+
+<style scoped>
+/* scoped让样式局部生效 ，防止取名重复*/
+.demo{
+  background-color: skyblue;
+}
+</style>
