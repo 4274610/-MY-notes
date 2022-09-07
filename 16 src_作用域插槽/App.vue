4c4addeb350d1556a997690e707ed4287845e810
yu-classmate <1914274610@qq.com>
2022-09-07 23:14:46 +0800

    作用域插槽

---
+++ 16 src_作用域插槽/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <Category title="美食">
+      <template v-slot="foodss">
+        <!-- 这里的foodss是一个对象,{foods:["火锅", "烧烤", "小龙虾", "牛排"]}名字可以随便命名 -->
+        <!-- v-slot和scope都可以,和具名插槽不一样slot="" -->
+        <ul>
+          <li v-for="(g, index) in foodss.foods" :key="index">{{ g }}</li>
+        </ul>
+      </template>
+    </Category>
+
+    <Category title="美食">
+      <!-- 解构赋值的写法{ foods:foods }={foods} -->
+      <template v-slot="{ foods:foods }">
+        <ol>
+          <li v-for="(g, index) in foods" :key="index">{{ g }}</li>
+        </ol>
+      </template>
+    </Category>
+
+    <Category title="美食">
+      <template v-slot="{ foods }">
+        <h4 v-for="(g, index) in foods" :key="index">{{ g }}</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+export default {
+  name: "App",
+  components: {
+    Category,
+  },
+};
+</script>
+<style>
+.container,
+.foot,
+h4 {
+  display: flex;
+  justify-content: space-around;
+}
+img {
+  width: 200px;
+  height: 100px;
+}
+video {
+  width: 200px;
+  height: 100px;
+}
+</style>
---
+++ 16 src_作用域插槽/components/Category.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <slot :foods="foods" >默认插槽，没有内容传递过来时，显示我</slot>
+    <!-- 数据传给插槽的使用者 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+  data() {
+    return {
+      foods: ["火锅", "烧烤", "小龙虾", "牛排"]
+    };
+  },
+};
+</script>
+
+<style scoped>
+.category {
+  background-color: rgb(187, 227, 214);
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: rgb(208, 182, 186);
+}
+</style>
