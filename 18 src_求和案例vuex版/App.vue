f7b674bdb4b9247fedc42cd71e53c6f1ab3bdbde
yu-classmate <1914274610@qq.com>
2022-09-09 23:20:10 +0800

    求和案例vuex版

---
+++ 18 src_求和案例vuex版/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <Count></Count>
+  </div>
+</template>
+
+<script>
+import  Count from "./components/Count.vue";
+export default {
+  name: "App",
+  components: {
+    Count
+  },
+};
+</script>
+<style>
+.container,
+.foot,
+h4 {
+  display: flex;
+  justify-content: space-around;
+}
+img {
+  width: 200px;
+  height: 100px;
+}
+video {
+  width: 200px;
+  height: 100px;
+}
+</style>
