cb950ed5a54c896c22f8a0aa951c5f4b1b982af3
yu-classmate <1914274610@qq.com>
2022-09-15 22:40:17 +0800

    router-link的replace属性

---
+++ 24 src_router-link的replace属性/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <h2>我是home组件</h2> -->
+
+  <div>
+    <ul class="nav nav-tabs">
+      <li>
+        <!-- 跳转路由要加上父级路径 -->
+        <router-link replace class="list-group-item" active-class="active" to="/home/news">News</router-link>
+      </li>
+      <li>
+        <router-link replace class="list-group-item " active-class="active" to="/home/message">Message</router-link>
+      </li>
+    </ul>
+    <div>
+      <ul>
+        <router-view></router-view>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style></style>
---
+++ 24 src_router-link的replace属性/pages/Message.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+     <!-- 用props测试params参数 -->
+      <!-- <li v-for="m in messageList" :key="m.id"><router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp;</li>  -->
+     
+     <!-- 用props测试query参数 -->   
+      <li v-for="m in messageList" :key="m.id"><router-link :to="`/home/message/detail?id=${m.id}&title=${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp;</li> 
+
+
+      <!-- 跳转路由并携带 params参数，to的对象写法 -->
+      <!-- <li v-for="m in messageList" :key="m.id">
+        <router-link
+          :to="{
+            name:'xiangqing',
+           //这里只能写name,不能写path
+            params: { id: m.id, title: m.title },
+          }"
+          >{{ m.title }}</router-link
+        >&nbsp;&nbsp;
+      </li> -->
+    </ul>
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageList: [
+        { id: "001", title: "消息001" },
+        { id: "002", title: "消息002" },
+        { id: "003", title: "消息003" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
