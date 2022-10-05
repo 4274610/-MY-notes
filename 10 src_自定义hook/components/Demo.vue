e4721d0ea83b0762fa67cca4ed65e442ba366d59
yu-classmate <1914274610@qq.com>
2022-10-05 22:45:09 +0800

    自定义hook

---
+++ 10 src_自定义hook/components/Demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>当前求和为：{{ sum }}</h1>
+    <button @click="sum++">点我n加一</button>
+    <h2>当前点击时鼠标的坐标为 x:{{ point.x }}y:{{ point.y }}</h2>
+  </div>
+</template>
+
+<script>
+import { onBeforeUnmount, onMounted, reactive, ref } from "vue";
+import usePoint from "../hooks/usePoint"
+export default {
+  name: "Demo",
+  setup() {
+    let sum = ref(1);
+    let point =usePoint()//调用封装的钩子
+    
+    return {
+      sum,
+      point
+
+    };
+  },
+};
+</script>
+
+<style></style>
